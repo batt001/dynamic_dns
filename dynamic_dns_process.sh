e131fb2390466e8ea05440d8bca88639ce67d384
batt001 <batt001.master@gmail.com>
2019-01-06 22:14:30 +0100

    Implement start/stop/status script

---
+++ dynamic_dns_process.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [[ "$1" = "start" ]]; then
+  pid=$(ps aux | grep 'python dynamic.dns.py' | grep -v 'grep' | awk '{print $2}')
+  if [[ "$pid" != "" ]]; then
+    echo "Dynamic DNS process is already running..."
+  else
+    nohup python dynamic_dns.py > /dev/null 2>&1 &
+    pid=$(ps aux | grep 'python dynamic.dns.py' | grep -v 'grep' | awk '{print $2}')
+    echo "Dynamic DNS process started with PID=$pid"
+  fi
+elif [[ "$1" = "stop" ]]; then
+  kill $(ps aux | grep 'python dynamic.dns.py' | grep -v 'grep' | awk '{print $2}')
+  echo "Dynamic DNS process killed..."
+elif [[ "$1" = "status" ]]; then
+  pid=$(ps aux | grep 'python dynamic.dns.py' | grep -v 'grep' | awk '{print $2}')
+  if [[ "$pid" = "" ]]; then
+    echo "Dynamic DNS process is not running..."
+  else
+    echo "Dynamic DNS process is running with PID=$pid"
+  fi
+else
+  echo "Wrong argument!"
+fi
--- run_dynamic_dns_process_background.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-nohup python dynamic_dns.py > /dev/null 2>&1 &
-pid=$(ps aux | grep 'python dynamic.dns.py' | grep -v 'grep' | awk '{print $2}')
-echo $pid
