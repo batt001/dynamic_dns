6ad0707030b74a21201b89707bac8c928c448008
sajtostejfolos <batt001.master@gmail.com>
2019-01-27 21:19:00 +0100

    replace python call with python3

--- dynamic_dns_process.sh
+++ dynamic_dns_process.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 if [[ "$1" = "start" ]]; then
-  pid=$(ps aux | grep 'python dynamic.dns.py' | grep -v 'grep' | awk '{print $2}')
+  pid=$(ps aux | grep 'python3 dynamic.dns.py' | grep -v 'grep' | awk '{print $2}')
   if [[ "$pid" != "" ]]; then
     echo "Dynamic DNS process is already running..."
   else
-    nohup python dynamic_dns.py > /dev/null 2>&1 &
-    pid=$(ps aux | grep 'python dynamic.dns.py' | grep -v 'grep' | awk '{print $2}')
+    nohup python3 dynamic_dns.py > /dev/null 2>&1 &
+    pid=$(ps aux | grep 'python3 dynamic.dns.py' | grep -v 'grep' | awk '{print $2}')
     echo "Dynamic DNS process started with PID=$pid"
   fi
 elif [[ "$1" = "stop" ]]; then
-  kill $(ps aux | grep 'python dynamic.dns.py' | grep -v 'grep' | awk '{print $2}')
+  kill $(ps aux | grep 'python3 dynamic.dns.py' | grep -v 'grep' | awk '{print $2}')
   echo "Dynamic DNS process killed..."
 elif [[ "$1" = "status" ]]; then
-  pid=$(ps aux | grep 'python dynamic.dns.py' | grep -v 'grep' | awk '{print $2}')
+  pid=$(ps aux | grep 'python3 dynamic.dns.py' | grep -v 'grep' | awk '{print $2}')
   if [[ "$pid" = "" ]]; then
     echo "Dynamic DNS process is not running..."
   else
