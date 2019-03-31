e31668324f394a32041a31393e7e0c28fac84666
sajtostejfolos <batt001.master@gmail.com>
2019-03-31 12:01:19 +0200

    Ignore idea IDE config folder

--- dynamic_dns.py
+++ dynamic_dns.py
@@ -5,69 +5,79 @@ import time
 import datetime
 import socket
 
+
 class dynamicDNS(threading.Thread):
 
-	def __init__(self):
-		threading.Thread.__init__(self)
-		self.username='batt001.master@gmail.com'
-		self.password='batt91221'
-		self.hostname='sajtostejfolos.ydns.eu'
-		self.delay=1800
-		self.previous_ip='not defined'
-		self.current_ip='not defined'
-		self.logfile='not defined'
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.username = 'batt001.master@gmail.com'
+        self.password = 'batt91221'
+        self.hostname = 'sajtostejfolos.ydns.eu'
+        self.delay = 1800
+        self.previous_ip = 'not defined'
+        self.current_ip = 'not defined'
+        self.logfile = 'not defined'
 
-	def run(self):
-		self.logfile = "dynamic_dns_" + datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S') + ".log"
-		while True:
-			if self.check_network_connection():
-				self.update_hostname()
-			time.sleep(self.delay)
+    def run(self):
+        self.logfile = "dynamic_dns_" + datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S') + ".log"
+        while True:
+            if self.check_network_connection():
+                self.update_hostname()
+            time.sleep(self.delay)
 
-	def update_hostname(self):
-		""" If IP address changed update the hostname with current IP"""
-		if (self.ip_changed()):
-			self.log("Ip changed, current ip: " + self.current_ip + " updating hostname: " + self.hostname + " ...")
+    def update_hostname(self):
+        """ If IP address changed update the hostname with current IP"""
+        if self.ip_changed():
+            self.log("Ip changed, current ip: " + self.current_ip + " updating hostname: " + self.hostname + " ...")
 
-			""" Send new ip to YDNS website """
-			update_ip_response = requests.get('https://ydns.io/api/v1/update/?host=' + self.hostname + '&ip=' + self.current_ip, auth=(self.username, self.password))
+            """ Send new ip to YDNS website """
+            requst_url = 'https://ydns.io/api/v1/update/?host=' + self.hostname + '&ip=' + self.current_ip
+            requests.get(requst_url, auth=(self.username, self.password))
 
-			""" Set previous_ip to current_ip for next iteration check """
-			self.previous_ip = self.current_ip
+            """ Set previous_ip to current_ip for next iteration check """
+            self.previous_ip = self.current_ip
 
-	""" Check if IP has changed """
-	def ip_changed(self):
-		get_current_ip_response = requests.get('https://ydns.io/api/v1/ip.json')
-		self.current_ip = json.loads(get_current_ip_response.text)['ip']
-		if (self.current_ip != self.previous_ip):
-			return True
-		else:
-			return False
 
-	""" Check if internet is avaliable """
-	def check_network_connection(self):
-		url_to_check = "ydns.io"
-		try:
-			socket.create_connection((url_to_check, 80))
-			return True
-		except OSError:
-			self.log("Network or " + url_to_check + " webpage is unavaliable")
-		return False
 
-	""" Writes the message passed as a parameter to the console and the defined logfile """
-	def log(self, message):
-		""" Setup log message """
-		timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " "
-		log_message = timestamp + message
+    def ip_changed(self):
+        # Check if IP has changed
+        get_current_ip_response = requests.get('https://ydns.io/api/v1/ip.json')
+        self.current_ip = json.loads(get_current_ip_response.text)['ip']
+        if self.current_ip != self.previous_ip:
+            return True
+        else:
+            return False
 
-		""" Pring log message to console """
-		print(log_message)
 
-		""" Write update event into logfile """
-		with open(self.logfile, 'a') as logfile:
-			logfile.write(log_message + "\n")
-			logfile.close()
+
+    def check_network_connection(self):
+        # Check if internet is available
+        url_to_check = "ydns.io"
+        try:
+            socket.create_connection((url_to_check, 80))
+            return True
+        except OSError:
+            self.log("Network or " + url_to_check + " webpage is unavailable")
+        return False
+
+
+
+    def log(self, message):
+        # Writes the message passed as a parameter to the console and the defined logfile
+
+        """ Setup log message """
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " "
+        log_message = timestamp + message
+
+        """ Pring log message to console """
+        print(log_message)
+
+        """ Write update event into logfile """
+        with open(self.logfile, 'a') as logfile:
+            logfile.write(log_message + "\n")
+            logfile.close()
+
 
 if __name__ == '__main__':
-	ddns_thread = dynamicDNS()
-	ddns_thread.start()
+    ddns_thread = dynamicDNS()
+    ddns_thread.start()
